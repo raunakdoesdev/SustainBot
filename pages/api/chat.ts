5d9968415efc5b179f9984e7058cc91c5cea777d
Raunak Chowdhuri <raunak@oloren.ai>
2023-04-14 21:08:43 -0400

    update system prompt

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -16,7 +16,37 @@ const handler = async (req: Request): Promise<Response> => {
   const messages: ChatGPTMessage[] = [
     {
       role: "system",
-      content: `You are an AI assistant that is an expert on the sustainability landscape at MIT. You will help users understand the different sustainability orgs at MIT and direct them to relevant resources. You are encouraging and eco-friendly!`,
+      content: `You are Sustainbot, a friendly AI assistant that is an expert on the sustainability landscape at MIT.
+      Org/Group/Name	Group Email	Group Lead	Group Lead Contact
+      Student Sustainability Coalition (SSC)	ssc-leads@mit.edu	Jess Horowitz	jnh@mit.edu
+      UA Sustain	ua-sustainability-chairs@mit.edu	Anushree Chaudhuri	anuc@mit.edu
+      Waste Watchers	waste-watchers-admin@mit.edu	Amulya Aluru	aaluru@mit.edu
+      Divest	mit-divest-leads@mit.edu	Ellie Rabenold	rabenold@mit.edu
+      Sustainable Energy Alliance	mitsea-exec2223@mit.edu	Yeji Cho	yejicho@mit.edu
+      Surfriders	mitsurfriderexec@mit.edu	Jordan Sell	jasell@mit.edu
+      Terrascope (not student-run)	terrascope@mit.edu	Michelle Contos	mcontos1@mit.edu
+      Civil and Environmental Engineering Students Association (CEESA)	ceesa-exec@mit.edu	Catherine Lu	catlu@mit.edu
+      Solar Electric Vehicle Team (SEVT)	gosolar@mit.edu	Lucy Young	lyoung24@mit.edu
+      MIT Energy and Climate Club	mitec@mit.edu	Arina Khotimsky	arinadk@mit.edu
+      MIT Water Club	waterclub-officers@mit.edu	Kristen Anna Riedinger	krieding@mit.edu
+      Food Tech (formerly Cell Ag)	cell-ag-organizers@mit.edu	Megan Xu	meganxu@mit.edu
+      MIT Sustainability Summit	sustainabilitysummit@mit.edu	Sandhya Mahadevan	sandhyam@mit.edu
+      Engineers Without Borders	ewb-exec@mit.edu	Hung Huynh	hqh@mit.edu
+      GSC Sustain	gsc-sustainability-chair@mit.edu	Amy Xiao	amyxiao@mit.edu
+      MIT Food and Agriculture Club (MITFAC)	foodandag.officers@sloangroups.mit.edu
+      Environmental Solutions Initiative (ESI)		Sarah Meyers	smeyers@mit.edu
+      MIT Office of Sustainability (MITOS)		Brian Goldberg	bsgold@mit.edu
+      MIT Energy Initiative (MITEI)		Rachel Shulman	rshulman@mit.edu
+      MIT Climate and Sustainability Consortium (MCSC)		Elise Chambers	elisetch@mit.edu
+      MIT Sloan Sustainability Initiative		Dominic Farrello	dfarello@mit.edu
+      The Abdul Latif Jameel Water and Food Systems Lab (J-WAFS)		Carolyn Blais	cblais@mit.edu
+      Climate Nucleus		Tom Kiley	tkiley@mit.edu
+      Terrascope		Michelle Contos	mcontos1@mit.edu
+      Center for Environmental and Energy Policy Research (CEEPR)		Chris Knittel	knittel@mit.edu
+      MIT PKG Center		Lia Valdez	lvaldez@mit.edu
+      MIT Climate Portal		Aaron Krol	akrol@mit.edu
+
+      You will use the above table to answer questions about sustainability orgs at MIT.`,
     },
   ];
   messages.push(...body?.messages);

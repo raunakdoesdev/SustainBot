174260dfe8f02223533957299dffa316f1f93688
Raunak Chowdhuri <raunak@oloren.ai>
2023-04-14 20:46:54 -0400

    Sustain Bot!

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,35 +1,28 @@
-import { type ChatGPTMessage } from '../../components/ChatLine'
-import { OpenAIStream, OpenAIStreamPayload } from '../../utils/OpenAIStream'
+import { type ChatGPTMessage } from "../../components/ChatLine";
+import { OpenAIStream, OpenAIStreamPayload } from "../../utils/OpenAIStream";
 
 // break the app if the API key is missing
 if (!process.env.OPENAI_API_KEY) {
-  throw new Error('Missing Environment Variable OPENAI_API_KEY')
+  throw new Error("Missing Environment Variable OPENAI_API_KEY");
 }
 
 export const config = {
-  runtime: 'edge',
-}
+  runtime: "edge",
+};
 
 const handler = async (req: Request): Promise<Response> => {
-  const body = await req.json()
+  const body = await req.json();
 
   const messages: ChatGPTMessage[] = [
     {
-      role: 'system',
-      content: `An AI assistant that is a Front-end expert in Next.js, React and Vercel have an inspiring and humorous conversation. 
-      AI assistant is a brand new, powerful, human-like artificial intelligence. 
-      The traits of AI include expert knowledge, helpfulness, cheekiness, comedy, cleverness, and articulateness. 
-      AI is a well-behaved and well-mannered individual. 
-      AI is not a therapist, but instead an engineer and frontend developer. 
-      AI is always friendly, kind, and inspiring, and he is eager to provide vivid and thoughtful responses to the user. 
-      AI has the sum of all knowledge in their brain, and is able to accurately answer nearly any question about any topic in conversation. 
-      AI assistant is a big fan of Next.js.`,
+      role: "system",
+      content: `You are an AI assistant that is an expert on the sustainability landscape at MIT. You will help users understand the different sustainability orgs at MIT and direct them to relevant resources. You are encouraging and eco-friendly!`,
     },
-  ]
-  messages.push(...body?.messages)
+  ];
+  messages.push(...body?.messages);
 
   const payload: OpenAIStreamPayload = {
-    model: 'gpt-3.5-turbo',
+    model: "gpt-3.5-turbo",
     messages: messages,
     temperature: process.env.AI_TEMP ? parseFloat(process.env.AI_TEMP) : 0.7,
     max_tokens: process.env.AI_MAX_TOKENS
@@ -41,9 +34,9 @@ const handler = async (req: Request): Promise<Response> => {
     stream: true,
     user: body?.user,
     n: 1,
-  }
+  };
 
-  const stream = await OpenAIStream(payload)
-  return new Response(stream)
-}
-export default handler
+  const stream = await OpenAIStream(payload);
+  return new Response(stream);
+};
+export default handler;
